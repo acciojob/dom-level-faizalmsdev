2a48c4499fa02d67702d809cc7a99122d9f8c723
MOHAMED FAIZAL M.S <108245146+faizalmsdev@users.noreply.github.com>
2023-06-20 19:37:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+window.onload = function() {
+  var element = document.getElementById("level");
+  var level = 0;
+
+  while (element.parentElement) {
+    element = element.parentElement;
+    level++;
+  }
+
+  var message = "The level of the element is: " + level;
+  alert(message);
+};
